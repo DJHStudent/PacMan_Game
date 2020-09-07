7e078fe1e01bee146164b36eae7ebbc9259ef188
DJHStudent <69775476+DJHStudent@users.noreply.github.com>
2020-09-07 22:20:52 +1000

    Added Sound Effects

---
+++ Assets/Scripts/DetermineSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetermineSound : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip normalClip;
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayAudio(normalClip);
+    }
+    void PlayAudio(AudioClip clip)
+    {
+        if(!audioSource.isPlaying)
+        {
+            audioSource.clip = normalClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
