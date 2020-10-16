b7c7dd900d7265fb6c2e51d4743d8a67c2cfec21
DJHStudent <69775476+DJHStudent@users.noreply.github.com>
2020-10-16 22:07:20 +1100

    Updated Cherry
    made cherry be instantiated and destroyed at appropriate times

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -8,39 +8,39 @@ public class CherryController : MonoBehaviour
     const float duration = 25f;
     bool moving = false;
     Vector2 startPos, endPos;
+
+    public GameObject cherryPrefab;
+    private GameObject cherryInstance;
     void Start()
     {
-        InvokeRepeating("startMovement", 1, 30); //do method every 30 seconds
+        InvokeRepeating("startMovement", 30, 30); //do method every 30 seconds
         Camera camera = Camera.main;
-
-        //float height = 2 * camera.orthographicSize;
-        //float width = height * camera.aspect;
     }
 
     void Update()
     {
-        if (moving == true) //if currently moving across the screen continue that movement and stop it when it reaches endPos
+        if (cherryInstance != null) //if currently moving across the screen continue that movement and stop it when it reaches endPos
         {
             changePos();
-            if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), tween.EndPos) < 0.05f)
+            if (Vector2.Distance(new Vector2(cherryInstance.transform.position.x, cherryInstance.transform.position.y), tween.EndPos) < 0.05f)
             {
                 moving = false;
-                gameObject.SetActive(false);
+                Destroy(cherryInstance.gameObject);
             }
         }
     }
 
-    void startMovement() //enable the cherry to move across the screen
+    void startMovement() //create the cherry to move across the screen
     {
-        gameObject.SetActive(true);
         startPos = Camera.main.ViewportToWorldPoint(new Vector2(1.1f, .5f)); //set the start and end points to be just outside the camera
         endPos = Camera.main.ViewportToWorldPoint(new Vector2(-0.1f, .5f));
+        cherryInstance = Instantiate(cherryPrefab, startPos, Quaternion.identity);//make the cherry
         tween = new Tween(startPos, endPos, Time.time, duration);
         moving = true; //tell update to check if finsihed movemenet or not
     }
     void changePos() //the actual movement of the cherry across the screen
     {
         float timeFraction = (Time.time - tween.StartTime) / tween.Duration;
-        transform.position = Vector2.Lerp(tween.StartPos, tween.EndPos, timeFraction);
+        cherryInstance.transform.position = Vector2.Lerp(tween.StartPos, tween.EndPos, timeFraction);
     }
 }
